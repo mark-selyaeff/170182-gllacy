0743f7b680d174f4a38b09b0edd448bc4ad2cf3d
-v <mark.selyaeff@yandex.ru>
2016-09-27 22:59:07 +0300

    Добавлены события открытия и закрытия формы обратной связи

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-var link = document.querySelector('.social-tw');
-link.addEventListener("click", function() {
-  event.preventDefault();
-  console.log('hi, im mark');
+var link = document.querySelector('.feedback-form');
+var popup = document.querySelector('.modal-content');
+var overlay = document.querySelector('.modal-overlay');
+var exit = document.querySelector('.modal-content-close');
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("modal-show");
+	overlay.classList.add("modal-show");
+});
+
+exit.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.remove("modal-show");
+	overlay.classList.remove("modal-show");
 });
