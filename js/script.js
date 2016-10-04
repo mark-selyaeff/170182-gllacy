7f4dbb6e3a37043efdaec33e26664e3165f52d71
-v <mark.selyaeff@yandex.ru>
2016-10-04 18:23:44 +0300

    JS для меню каталога

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var link = document.querySelector('.feedback-form');
 var popup = document.querySelector('.modal-content');
 var overlay = document.querySelector('.modal-overlay');
 var exit = document.querySelector('.modal-content-close');
-var catalog = document.querySelector('.main-menu ul > li:first-child');
+var catalog = document.querySelector('.main-menu ul > li:first-child a');
 var catalog_popup = document.querySelector('.catalog-modal-content');
 
 link.addEventListener("click", function(event) {
@@ -17,6 +17,7 @@ exit.addEventListener("click", function(event) {
 	overlay.classList.remove("modal-show");
 });
 
-catalog.addEventListener("click", function(event) {
+catalog.addEventListener("mouseover", function(event) {
+	event.preventDefault();
 	catalog_popup.classList.add("modal-show");
 });
