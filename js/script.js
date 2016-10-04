83041c934befa1f924f897a15374c1fcdc02b033
-v <mark.selyaeff@yandex.ru>
2016-10-04 12:04:30 +0300

    Добавил модальное меню Каталога

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var link = document.querySelector('.feedback-form');
 var popup = document.querySelector('.modal-content');
 var overlay = document.querySelector('.modal-overlay');
 var exit = document.querySelector('.modal-content-close');
+var catalog = document.querySelector('.main-menu ul > li:first-child');
+var catalog_popup = document.querySelector('.catalog-modal-content');
 
 link.addEventListener("click", function(event) {
 	event.preventDefault();
@@ -13,4 +15,8 @@ exit.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.remove("modal-show");
 	overlay.classList.remove("modal-show");
+});
+
+catalog.addEventListener("click", function(event) {
+	catalog_popup.classList.add("modal-show");
 });
